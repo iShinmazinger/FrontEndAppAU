760cb921b4fc1c9ff811b7a63da2854067bacc3a
iShinmazinger <mazinger05x72@gmail.com>
2025-10-19 23:31:26 -0500

    Correcciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backappau-ia.onrender.com'
+  apiUrl: 'http://localhost:4000'
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideRouter } from '@angular/router';
+import { provideRouter,withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,7 +11,7 @@ import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom(BrowserAnimationsModule),
   ]
